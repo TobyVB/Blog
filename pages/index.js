5e4e1d1f9842ff7370ceb5ab70ce53b618b59d27
TobyVB <98196156+TobyVB@users.noreply.github.com>
2023-03-01 12:21:09 +0800

    add dynamic routes

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
+import Date from "../components/date";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -34,16 +35,15 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
       </section>
-      <Link href="/posts/first-post">first post</Link>
     </Layout>
   );
 }
